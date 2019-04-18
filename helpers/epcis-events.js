33c4efbe9900b8405a97ce12e4921272ada97012
Marius Lichtblau <marius.lichtblau@googlemail.com>
2019-04-18 18:38:47 +0200

    ✨ Send SIS interface relevant information when event occurs

---
+++ helpers/epcis-events.js
@@ -0,0 +1,143 @@
+const request = require('request-promise');
+const xmlbuilder = require('xmlbuilder');
+
+const SIS_BASE_URL = 'https://smile-dev.epcat.de';
+const SIS_EVENT_URL = `${ SIS_BASE_URL }/api/capture/`;
+const SIS_USERNAME = 'marius.lichtblau@googlemail.com';
+const SIS_PASSWORD = 'Anjo1998';
+
+const getCurrentTimeAsIsoString = () => {
+  const currentDate = new Date();
+  return currentDate.toISOString();
+};
+
+const wrapObjectEvent = function (objectEvent) {
+  return {
+    'epcis:EPCISDocument': {
+      '@xmlns:epcis': 'urn:epcglobal:epcis:xsd:1',
+      '@xmlns:epcglobal': 'urn:epcglobal:xsd:1',
+      '@xmlns:smile': 'http://ns.smile-project.de/epcis',
+      '@schemaVersion': '1.2',
+      '@creationDate': '2018-05-15T14:07:44.834Z',
+      'EPCISBody': {
+        'EventList': {
+          'ObjectEvent': objectEvent
+        }
+      }
+    }
+  }
+};
+
+const getEventXml = function (objectEvent) {
+  const eventJSON = wrapObjectEvent(objectEvent);
+  return xmlbuilder.create(eventJSON).end({ pretty: true });
+};
+
+const receiving = function (sscc, depotId) {
+  return getEventXml({
+    'eventTime': {
+      '#text': getCurrentTimeAsIsoString()
+    },
+    'eventTimeZoneOffset': {
+      '#text': '+02:00'
+    },
+    'epcList': {
+      'epc': {
+        '#text': sscc
+      }
+    },
+    'action': {
+      '#text': 'OBSERVE'
+    },
+    'bizStep': {
+      '#text': 'urn:epcglobal:cbv:bizstep:receiving'
+    },
+    'bizLocation': {
+      'id': {
+        '#text': depotId
+      }
+    }
+  });
+};
+
+const shipping = function (sscc, depotId, receiverId) {
+  return getEventXml({
+    'eventTime': {
+      '#text': getCurrentTimeAsIsoString()
+    },
+    'eventTimeZoneOffset': {
+      '#text': '+02:00'
+    },
+    'epcList': {
+      'epc': {
+        '#text': sscc
+      }
+    },
+    'action': {
+      '#text': 'OBSERVE'
+    },
+    'bizStep': {
+      '#text': 'urn:epcglobal:cbv:bizstep:shipping'
+    },
+    'disposition': {
+      '#text': 'urn:epcglobal:cbv:disp:in_transit'
+    },
+    'bizLocation': {
+      'id': {
+        '#text': depotId
+      }
+    },
+    'smile:personId': [
+      { '#text': 'fahrerID' },
+      { '#text': receiverId }
+    ]
+  });
+};
+
+const receiving2 = function (sscc, receiverId) {
+  return getEventXml({
+    'eventTime': {
+      '#text': getCurrentTimeAsIsoString()
+    },
+    'eventTimeZoneOffset': {
+      '#text': '+02:00'
+    },
+    'epcList': {
+      'epc': {
+        '#text': sscc
+      }
+    },
+    'action': {
+      '#text': 'OBSERVE'
+    },
+    'bizStep': {
+      '#text': 'urn:epcglobal:cbv:bizstep:receiving'
+    },
+    'bizLocation': {
+      'id': {
+        '#text': 'fahrerId'
+      }
+    },
+    'smile:personId': {
+      '#text': receiverId
+    }
+  });
+};
+
+const send = function (eventXml) {
+  const headers = { 'Content-Type': 'application/xml' };
+  return request.post(SIS_EVENT_URL, {
+    headers,
+    body: eventXml,
+    auth: { 'user': SIS_USERNAME, 'pass': SIS_PASSWORD }
+  })
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+};
+
+module.exports = {
+  receiving,
+  shipping,
+  receiving2,
+  send
+};
--- routes/sis.js
+++ routes/sis.js
@@ -1,17 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var helpers = require('../helpers');
+var epcisEvents = require('../helpers/epcis-events');
 
 router.post('/arrived-at-depot', function (req, res, next) {
+  const { sscc, depotID } = req.body;
+  const eventXml = epcisEvents.receiving(sscc, depotID);
+  epcisEvents.send(eventXml);
   res.json({ message: "Event received." });
 });
 
 router.post('/pickup-reported', function (req, res, next) {
+  const { sscc, depotID, receiverID } = req.body;
+  const eventXml = epcisEvents.shipping(sscc, depotID, receiverID);
+  epcisEvents.send(eventXml);
   res.json({ message: "Event received." });
 });
 
 router.post('/delivery-reported', function (req, res, next) {
-  res.json({ message: "Event received."});
+  const { sscc, receiverID } = req.body;
+  const eventXml = epcisEvents.receiving2(sscc, receiverID);
+  epcisEvents.send(eventXml);
+  res.json({ message: "Event received." });
 });
 
 /* POST new sis event list */

f8a05dd51cd4811947cf4c95abde2d2c1c2bc93f
Marius Lichtblau <marius.lichtblau@googlemail.com>
2019-05-07 14:49:23 +0200

    🐛 Only create events in Unicorn when bizStep is commissioning

---
+++ helpers/unicorn-events.js
@@ -0,0 +1,97 @@
+const ETParcelDataCreated = (sisEvent) => {
+  const {
+    eventTime,
+    eventTimeZoneOffset,
+    epcList: { epc },
+    action,
+    bizStep,
+    disposition,
+    readPoint: { id: readPointId } = { },
+    bizLocation: { id: bizLocationId } = { },
+    extension: {
+      ilmd: {
+        'smile:sender': {
+          'smile:city': senderCity,
+          'smile:country': senderCountry,
+          'smile:firstName': senderFirstname,
+          'smile:lastName': senderLastname,
+          'smile:gln': senderGLN,
+          'smile:streetName': senderStreetName,
+          'smile:streetNumber': senderStreetNumber,
+          'smile:zip': senderZip,
+          'smile:organisation': senderOrganisation
+        } = { },
+        'smile:depot': {
+          'smile:city': depotCity,
+          'smile:country': depotCountry,
+          'smile:organisation': depotOrganisation,
+          'smile:streetName': depotStreetName,
+          'smile:streetNumber': depotStreetNumber,
+          'smile:zip': depotZip
+        } = { },
+        'smile:receiver': {
+          'smile:firstName': receiverFirstname,
+          'smile:lastName': receiverLastname,
+          'smile:smileUserId': receiverSmileUserId
+        } = { },
+        'smile:item': {
+          'smile:grossWeight': {
+            'smile:uom': grossWeightUOM,
+            'smile:value': grossWeightValue
+          } = { },
+          'smile:height': {
+            'smile:uom': heightUOM,
+            'smile:value': heightValue
+          } = { },
+          'smile:width': {
+            'smile:uom': widthUOM,
+            'smile:value': widthValue
+          } = { },
+          'smile:length': {
+            'smile:uom': lengthUOM,
+            'smile:value': lengthValue
+          }
+        } = { },
+        'smile:dateOfPlannedDelivery': {
+          _: dateOfPlannedDelivery
+        } = { },
+        'smile:carrierItemId': {
+          _: carrierItemId
+        } = { }
+      }
+    }
+  } = sisEvent.ObjectEvent;
+  return {
+    "initialSendDate": eventTime,
+    "length": lengthValue,
+    "dateOfPlannedDelivery": dateOfPlannedDelivery,
+    "depotZIP": depotZip,
+    "receiverLastName": receiverLastname,
+    "receiverID": receiverSmileUserId,
+    "depotID": "partyhuetchen",
+    "height": heightValue,
+    "senderStreetNumber": senderStreetNumber,
+    "senderFirstName": senderFirstname,
+    "senderZIP": senderZip,
+    "depotCity": depotCity,
+    "depotStreetNumber": depotStreetNumber,
+    "width": widthValue,
+    "receiverFirstName": receiverFirstname,
+    "depotStreetName": depotStreetName,
+    "sscc": epc,
+    "senderOrganization": senderOrganisation,
+    "depotCountry": depotCountry,
+    "senderOrganizationGLN": senderGLN,
+    "senderCity": senderCity,
+    "bizStep": bizStep,
+    "senderStreetName": senderStreetName,
+    "depotName": depotOrganisation,
+    "grossWeight": grossWeightValue,
+    "senderLastName": senderLastname,
+    "senderCountry": senderCountry
+  };
+};
+
+module.exports = {
+  ETParcelDataCreated
+};
--- routes/sis.js
+++ routes/sis.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var helpers = require('../helpers');
 var epcisEvents = require('../helpers/epcis-events');
+var parseXmlString = require('xml2js').parseString;
+var unicornAdapter = require('../unicorn/unicorn').unicornAdapter;
 
 /* UNICORN subscriptions */
 
@@ -37,16 +39,45 @@ router.post('/parcels', function (req, res, next) {
     // match ObjectEvents
     let match, objectEventRegExp = /(<ObjectEvent>[\s\S]*?<\/ObjectEvent>)/g;
     let $results = [];
-    // send every ObjectEvent match to Unicorn
     while ((match = objectEventRegExp.exec(payload)) !== null) {
-      const objectEvent = match[0];
-      const queryDocument = helpers.getEPCISQueryDocumentForObjectEvent(objectEvent);
-      $results.push(helpers.sendEventToUnicorn(queryDocument));
+      const objectEventXml = match[0];
+      // try parsing object events
+      const $objectEvent = new Promise(((resolve, reject) => {
+        parseXmlString(objectEventXml, {explicitArray : false}, (err, result) => {
+          if (err) reject(err);
+          else resolve(result);
+        });
+      }));
+      const $result = $objectEvent.then(result => {
+        const bizStepName = result.ObjectEvent.bizStep;
+        switch (bizStepName) {
+          // Creation of parcel data
+          case 'urn:epcglobal:cbv:bizstep:commissioning':
+            // convert SIS event to ETParcelDataCreated
+            const unicornEvent = helpers.unicornEvents.ETParcelDataCreated(result);
+            return unicornAdapter.generateChimeraEvent(unicornEvent, 'ETParcelDataReceived', 'new');
+          // Parcel arrives at depot or at the receiver
+          case 'urn:epcglobal:cbv:bizstep:receiving':
+            break;
+          // Parcel is picked up at depot
+          case 'urn:epcglobal:cbv:bizstep:shipping':
+            break;
+          default:
+            break;
+        }
+      });
+      $results.push($result);
     }
     // Send response
     Promise.all($results)
-      .then(results => res.json({ status: 'success' }))
-      .catch(error => res.status(500).json({ status: 'failed 😢', error: error }));
+      .then(results => {
+        console.info('Imported events: ' + results.join(", "));
+        res.json({ status: 'success', results });
+      })
+      .catch(error => {
+        console.error('Failed to import events:\n' + error);
+        res.status(500).json({ status: 'failed 😢', error });
+      });
   }
 });
 

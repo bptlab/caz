faf697f9bfd9ef85ab869c9466c0f9ac0fd4a930
Marius Lichtblau <marius.lichtblau@googlemail.com>
2019-05-16 15:20:03 +0200

    🔧 Allow CORS

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.text({ type: ['text/*', 'application/*'] }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/sis', sisRouter);
 app.use('/pickshare', pickshareRouter);
